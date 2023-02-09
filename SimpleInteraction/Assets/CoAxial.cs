ea93e234e07aaab8737534f23cfbe15940571444
Che Zhihui <c.zhihui@iaw.rwth-aachen.de>
2023-02-09 16:40:24 +0100

    0902 raycast done

---
+++ SimpleInteraction/Assets/CoAxial.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoAxial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        int layerMask = (1 << 0)|(1 << 3);
+
+        RaycastHit hit;
+        GameObject gameObject;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up),out hit, Mathf.Infinity,layerMask))
+        {
+            Debug.Log("Did Hit");
+            gameObject = hit.transform.gameObject;
+            gameObject.GetComponent<Rigidbody>().useGravity = false;
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.transform.position = transform.position ;
+            //+Vector3.Dot(gameObject.transform.position, Vector3.up) * Vector3.up
+
+        }
+        else
+        {
+            Debug.Log("Did not Hit");
+        }
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 10, Color.yellow);
+    }
+}
