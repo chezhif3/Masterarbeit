999d92b86a45a9e57b645bf57808e9f4cc8d3d16
chezhif3 <47634060+chezhif3@users.noreply.github.com>
2023-02-10 02:01:21 +0100

    linear drive implement

--- SimpleInteraction/Assets/CoAxial.cs
+++ SimpleInteraction/Assets/CoAxial.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CoAxial : MonoBehaviour
 {
+    bool captured = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +20,20 @@ public class CoAxial : MonoBehaviour
         RaycastHit hit;
         GameObject gameObject;
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up),out hit, Mathf.Infinity,layerMask))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up),out hit, Mathf.Infinity,layerMask)&&(!captured))
         {
             Debug.Log("Did Hit");
             gameObject = hit.transform.gameObject;
             gameObject.GetComponent<Rigidbody>().useGravity = false;
             gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            gameObject.transform.position = transform.position ;
-            //+Vector3.Dot(gameObject.transform.position, Vector3.up) * Vector3.up
+            gameObject.transform.position = transform.position + Vector3.Dot((gameObject.transform.position- transform.position), transform.TransformDirection(Vector3.up)) * Vector3.Normalize(transform.TransformDirection(Vector3.up));
+            captured = true;
 
         }
-        else
-        {
-            Debug.Log("Did not Hit");
-        }
+        //else
+        //{
+        //    Debug.Log("Did not Hit");
+        //}
 
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 10, Color.yellow);
     }
